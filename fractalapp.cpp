178a6ec3a23c0d2b6450def05947f747a4568da3
Michał <michalurb8@wp.pl>
2021-05-04 01:01:41 +0200

    Test if repo broken

--- fractalapp.cpp
+++ fractalapp.cpp
@@ -15,6 +15,7 @@ FractalApp::~FractalApp()
 
 void FractalApp::on_sliderPoints_valueChanged(int value)
 {
+
     ui->sliderStep->setRange(0,value-1);
     ui->sliderOffset->setRange(0,value-1);
     ui->spinStep->setRange(0,value-1);
